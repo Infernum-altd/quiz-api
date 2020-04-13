ed181a69e09ead3def0e6138c53925aed7fc3614
Taya <justataya@gmail.com>
2020-04-13 13:22:23 -0700

    Add script to create quizzes table

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -1,4 +1,10 @@
+DROP TABLE IF EXISTS countries;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS quizzes;
+DROP TABLE IF EXISTS favorite_quizzes;
+DROP TABLE IF EXISTS score;
+DROP TABLE IF EXISTS questions;
+DROP TABLE IF EXISTS answers;
 
 CREATE TYPE gender_type AS ENUM ('MALE','FEMALE','NOT_MENTIONED');
 CREATE TYPE role_type AS ENUM ('USER', 'MODERATOR', 'ADMIN','SUPER_ADMIN');
@@ -11,13 +17,14 @@ CREATE TABLE countries
     name VARCHAR(255) UNIQUE NOT NULL
 );
 
-CREATE TABLE USERS
+CREATE TABLE users
 (
     id            serial PRIMARY KEY,
     email         VARCHAR(255) UNIQUE    NOT NULL,
     role          role_type              NOT NULL DEFAULT 'USER',
     name          VARCHAR(50)            NOT NULL,
     surname       VARCHAR(50),
+    image         BYTEA,
     birthdate     DATE                   NOT NULL,
     gender        gender_type            NOT NULL DEFAULT 'NOT_MENTIONED',
     country_id    INTEGER REFERENCES countries (id),
@@ -26,4 +33,53 @@ CREATE TABLE USERS
     about         TEXT,
     active        BOOLEAN                NOT NULL DEFAULT true,
     notifications user_notification_type NOT NULL DEFAULT 'ON'
+);
+
+CREATE TYPE quiz_status_type AS ENUM ('PENDING','ACTIVE','DEACTIVATED', 'DELETED');
+
+CREATE TABLE quizzes
+(
+    id                serial PRIMARY KEY,
+    name              VARCHAR(255)                  NOT NULL,
+    image             BYTEA,
+    author            INTEGER REFERENCES users (id) NOT NULL,
+    date              DATE                          NOT NULL,
+    description       TEXT,
+    status            quiz_status_type              NOT NULL DEFAULT 'PENDING',
+    modification_time TIMESTAMP
+);
+
+CREATE TABLE favorite_quizzes
+(
+    user_id INTEGER REFERENCES users (id)  NOT NULL,
+    quiz_id INTEGER REFERENCES quizes (id) NOT NULL
+);
+
+CREATE TABLE score
+(
+    user_id INTEGER REFERENCES users (id)  NOT NULL,
+    quiz_id INTEGER REFERENCES quizes (id) NOT NULL,
+    score   INTEGER                        NOT NULL
+);
+
+
+
+CREATE TYPE question_type AS ENUM ('OPTION','BOOLEAN','ANSWER','SEQUENCE');
+
+CREATE TABLE questions
+(
+    id      serial PRIMARY KEY,
+    quiz_id INTEGER REFERENCES quizzes (id),
+    type    question_type NOT NULL,
+    image   BYTEA,
+    text    TEXT
+);
+
+CREATE TABLE answers
+(
+    id             serial PRIMARY KEY,
+    question_id    INTEGER REFERENCES questions (id),
+    text           TEXT,
+    correct        BOOLEAN,
+    next_answer_id INTEGER REFERENCES answers (id)
 );
