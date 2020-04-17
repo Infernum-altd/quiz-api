bf9c05dcd2a322f1d1735c8c1725308de6648976
avigeris <dascha2611@gmail.com>
2020-04-17 04:20:16 +0300

    added some functionality for interacting with quiz

--- src/main/java/com/quiz/service/QuizService.java
+++ src/main/java/com/quiz/service/QuizService.java
@@ -2,6 +2,7 @@ package com.quiz.service;
 
 import com.quiz.dao.QuizDao;
 import com.quiz.entities.Quiz;
+import com.quiz.entities.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -13,11 +14,22 @@ public class QuizService {
 
     private final QuizDao quizDao;
 
+    public Quiz findById(int id) { return quizDao.findById(id); }
+
     public List<Quiz> findQuizzesCreatedByUserId(int userId) {
-        return quizDao.getGamesCreatedByUser(userId);
+        return quizDao.getQuizzesCreatedByUser(userId);
     }
 
     public List<Quiz> findFavoriteQuizzes(int userId) {
-        return quizDao.getFavoriteGamesByUserId(userId);
+        return quizDao.getFavoriteQuizzesByUserId(userId);
     }
+
+    public List<Quiz> findQuizzesByCategory(int categoryId) {
+        return quizDao.getQuizzesByCategory(categoryId);
+    }
+
+    public boolean updateQuiz(Quiz quiz) {
+        return quizDao.updateQuiz(quiz);
+    }
+
 }
