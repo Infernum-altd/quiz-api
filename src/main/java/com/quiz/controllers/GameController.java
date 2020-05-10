24e101e3604973a89a80ae829eb48d3f68ada241
Infernum_altd <altd0606@gmail.com>
2020-05-10 15:02:47 +0300

    "add finish mapping"

--- src/main/java/com/quiz/controllers/GameController.java
+++ src/main/java/com/quiz/controllers/GameController.java
@@ -3,16 +3,11 @@ package com.quiz.controllers;
 import com.quiz.dto.GameAnswersDto;
 import com.quiz.dto.GameQuestionsDto;
 import com.quiz.dto.GameSessionDto;
-import com.quiz.entities.Answer;
-import com.quiz.entities.GameSession;
 import com.quiz.entities.Player;
 import com.quiz.service.GameService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
-import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.annotation.SubscribeMapping;
 import org.springframework.stereotype.Controller;
@@ -25,23 +20,7 @@ import java.util.Set;
 @RequiredArgsConstructor
 public class GameController {
     private GameService gameService;
-    //private SimpMessageSendingOperations messagingTemplate;
     private SimpMessagingTemplate template;
-
-
-
-//private final SimpMessagingTemplate template;
-
-/*    @Autowired
-    GameController(SimpMessagingTemplate template){
-        this.template = template;
-    }
-
-    @MessageMapping("/send/message")
-    public void sendMessage(String message){
-        System.out.println(message);
-        this.template.convertAndSend("/message",  message);
-    }*/
 
     @PostMapping("/play/addSession")
     public GameSessionDto addGameSession(GameSessionDto gameSessionDto) {
@@ -54,21 +33,24 @@ public class GameController {
         return gameService.addUserInSession(gameId, userId);
     }
 
-/*    @MessageMapping("/play/{gameId}/sendQuestion")
-    public GameQuestionsDto sendQuestion(@DestinationVariable int gameId) {
-        return this.gameService.nextQuestion(gameId);
-    }*/
+    @MessageMapping("/play/{gameId}/start")
+    public void startGame(@DestinationVariable int gameId) {
+        this.sendQuestion(gameId, this.gameService.nextQuestion(gameId));
+    }
 
-    @MessageMapping("play/{gameId}/{userId}/getAnswer")
-    @SendTo("/play/{gameId}")
-    public GameQuestionsDto getAnswer(@DestinationVariable int gameId, @DestinationVariable int userId, GameAnswersDto answers) {
+    @MessageMapping("play/{gameId}/{userId}/sendAnswer")
+    public void receiveAnswer(@DestinationVariable int gameId, @DestinationVariable int userId, GameAnswersDto answers) {
         this.gameService.handleAnswer(gameId, userId, answers);
-        return this.gameService.nextQuestion(gameId);
+        this.sendQuestion(gameId, this.gameService.nextQuestion(gameId));
     }
 
     @MessageMapping("/play/{gameId}/finish")
     public Set<Player> finishGame(@DestinationVariable int gameId){
         return gameService.deleteGameSession(gameId);
+    }
+
+    private void sendQuestion(int gameId, GameQuestionsDto gameQuestionsDto){
+        template.convertAndSend("/play/" + gameId, gameQuestionsDto);
     }
 
 }
