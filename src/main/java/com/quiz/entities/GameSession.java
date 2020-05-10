250dcb6e192b89384bd803a70db1d6742f1133b6
Taya <justataya@gmail.com>
2020-05-09 18:40:25 -0700

    Fix game session

--- src/main/java/com/quiz/entities/GameSession.java
+++ src/main/java/com/quiz/entities/GameSession.java
@@ -3,30 +3,29 @@ package com.quiz.entities;
 import com.quiz.dto.GameQuestionsDto;
 import lombok.Data;
 
-
 import java.util.List;
-
 import java.util.Set;
-import java.util.TreeSet;
+import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.concurrent.atomic.LongAdder;
 
 @Data
 public class GameSession {
-    private int hostId;
-    private List<Question> questions;
-    private volatile Set<Player> playerSet = new TreeSet<>();
+    private final int hostId;
+    private final List<Question> questions;
+    private Set<Player> playerSet = new ConcurrentSkipListSet<>();
     private int currentQuestion;
-    private int collectedAnswers;
+    private LongAdder collectedAnswers;
 
     public GameSession(int hostId, List<Question> questions) {
         this.hostId = hostId;
         this.questions = questions;
         this.currentQuestion = 0;
-        this.collectedAnswers = 0;
+        this.collectedAnswers = new LongAdder();
     }
 
-    public GameQuestionsDto nextQuestion(){
-        if (this.collectedAnswers == this.playerSet.size()) {
-            this.collectedAnswers = 0;
+    public synchronized GameQuestionsDto nextQuestion() {
+        if (this.collectedAnswers.intValue() == this.playerSet.size()) {
+            this.collectedAnswers.reset();
             this.currentQuestion++;
         }
 
@@ -34,20 +33,19 @@ public class GameSession {
             return null;
         }
 
-        return new GameQuestionsDto(this.currentQuestion ,questions.get(this.currentQuestion));
+        return new GameQuestionsDto(this.currentQuestion, questions.get(this.currentQuestion));
     }
 
-    public void addScorePoint(int score, int userId){
-        this.playerSet.forEach(
-                player -> {
-                    if (player.getUserId() == userId) {
-                        player.setUserScore(player.getUserScore() + score);
-                    }
-                }
-        );
+    public void addScorePoint(int score, int userId) {
+        for (Player player : this.playerSet) {
+            if (player.getUserId() == userId) {
+                player.setUserScore(player.getUserScore() + score);
+            }
+        }
     }
 
-    public int incrementCollectedAnswer(){
-        return ++this.collectedAnswers;
+    public int incrementCollectedAnswer() {
+        this.collectedAnswers.increment();
+        return this.collectedAnswers.intValue();
     }
 }
